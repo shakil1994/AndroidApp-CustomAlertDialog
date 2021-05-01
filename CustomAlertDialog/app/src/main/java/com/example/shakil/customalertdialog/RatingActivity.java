da19878b313a533e6e875e7bab6e056dd440de7b
android_shakil <shakil!@#$1234>
2021-05-01 15:46:00 +0600

    [Added] Bottom Navigation View

--- CustomAlertDialog/app/src/main/java/com/example/shakil/customalertdialog/RatingActivity.java
+++ CustomAlertDialog/app/src/main/java/com/example/shakil/customalertdialog/RatingActivity.java
@@ -1,14 +1,8 @@
 package com.example.shakil.customalertdialog;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.LayerDrawable;
-import android.os.Build;
 import android.os.Bundle;
-import android.widget.RatingBar;
 
 public class RatingActivity extends AppCompatActivity {
 
@@ -16,9 +10,5 @@ public class RatingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating);
-
-        /*RatingBar ratingBar = (RatingBar) findViewById(R.id.rating_bar);
-        LayerDrawable stars = (LayerDrawable) ratingBar.getProgressDrawable();
-        stars.getDrawable(0).setColorFilter(getColor(R.color.rating), PorterDuff.Mode.SRC_ATOP);*/
     }
 }
