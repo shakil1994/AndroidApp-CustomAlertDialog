d20e5eef4c01e8a97402a938476c364e38e70116
android_shakil <shakil!@#$1234>
2021-05-02 12:37:37 +0600

    [Added] Design Home Activity and item click listener

--- CustomAlertDialog/app/src/main/java/com/example/shakil/customalertdialog/HomeActivity.java
+++ CustomAlertDialog/app/src/main/java/com/example/shakil/customalertdialog/HomeActivity.java
@@ -1,14 +1,24 @@
 package com.example.shakil.customalertdialog;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
+
+    CardView cardViewServices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        cardViewServices = findViewById(R.id.cardViewServices);
+        cardViewServices.setOnClickListener(v -> {
+            startActivity(new Intent(this, ServicesActivity.class));
+        });
     }
 }
--- CustomAlertDialog/app/src/main/java/com/example/shakil/customalertdialog/ServicesActivity.java
+++ CustomAlertDialog/app/src/main/java/com/example/shakil/customalertdialog/ServicesActivity.java
@@ -3,22 +3,16 @@ package com.example.shakil.customalertdialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
 
-public class RatingActivity extends AppCompatActivity {
+public class ServicesActivity extends AppCompatActivity {
 
-    TextView toolbarText;
     ImageView imgBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rating);
-
-        toolbarText = findViewById(R.id.toolbarText);
-        toolbarText.setVisibility(View.GONE);
+        setContentView(R.layout.activity_services);
 
         imgBack = findViewById(R.id.imgBack);
         imgBack.setOnClickListener(v -> {
