e9be322b229da2c4a15933f5696b53133fce3283
android_shakil <shakil!@#$1234>
2021-05-03 17:17:03 +0600

    [Added] Payment History layout Design

--- CustomAlertDialog/app/src/main/java/com/example/shakil/customalertdialog/AddDiscountCouponActivity.java
+++ CustomAlertDialog/app/src/main/java/com/example/shakil/customalertdialog/AddDiscountCouponActivity.java
@@ -2,20 +2,29 @@ package com.example.shakil.customalertdialog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class AddDiscountCouponActivity extends AppCompatActivity {
 
     ImageView imgBack, imgFilter;
     TextView toolbarText;
 
+    EditText edtStartDate, edtEndDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_discount_coupon);
+
+        edtStartDate = findViewById(R.id.edtStartDate);
+        edtEndDate = findViewById(R.id.edtEndDate);
 
         imgFilter = findViewById(R.id.imgFilter);
         imgFilter.setVisibility(View.GONE);
@@ -27,5 +36,28 @@ public class AddDiscountCouponActivity extends AppCompatActivity {
 
         toolbarText = findViewById(R.id.toolbarText);
         toolbarText.setText("Discount Coupon");
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        edtStartDate.setOnClickListener(v -> {
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, (view, year1, month1, dayOfMonth) -> {
+                month1 = month1 + 1;
+                String date = dayOfMonth + "/" + month1 + "/" + year1;
+                edtStartDate.setText(date);
+            }, year, month, day);
+            datePickerDialog.show();
+        });
+
+        edtEndDate.setOnClickListener(v -> {
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, (view, year1, month1, dayOfMonth) -> {
+                month1 = month1 + 1;
+                String date = dayOfMonth + "/" + month1 + "/" + year1;
+                edtEndDate.setText(date);
+            }, year, month, day);
+            datePickerDialog.show();
+        });
     }
 }
--- CustomAlertDialog/app/src/main/java/com/example/shakil/customalertdialog/PaymentHistoryFilterActivity.java
+++ CustomAlertDialog/app/src/main/java/com/example/shakil/customalertdialog/PaymentHistoryFilterActivity.java
@@ -2,21 +2,31 @@ package com.example.shakil.customalertdialog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class PaymentHistoryFilterActivity extends AppCompatActivity {
 
     ImageView imgBack, imgFilter;
     TextView toolbarText;
 
+    EditText edtStartDate, edtEndDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_history_filter);
+
+        edtStartDate = findViewById(R.id.edtStartDate);
+        edtEndDate = findViewById(R.id.edtEndDate);
 
         imgFilter = findViewById(R.id.imgFilter);
         imgFilter.setVisibility(View.GONE);
@@ -28,5 +38,28 @@ public class PaymentHistoryFilterActivity extends AppCompatActivity {
 
         toolbarText = findViewById(R.id.toolbarText);
         toolbarText.setText("Filter");
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        edtStartDate.setOnClickListener(v -> {
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, (view, year1, month1, dayOfMonth) -> {
+                month1 = month1 + 1;
+                String date = dayOfMonth + "/" + month1 + "/" + year1;
+                edtStartDate.setText(date);
+            }, year, month, day);
+            datePickerDialog.show();
+        });
+
+        edtEndDate.setOnClickListener(v -> {
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, (view, year1, month1, dayOfMonth) -> {
+                month1 = month1 + 1;
+                String date = dayOfMonth + "/" + month1 + "/" + year1;
+                edtEndDate.setText(date);
+            }, year, month, day);
+            datePickerDialog.show();
+        });
     }
 }
