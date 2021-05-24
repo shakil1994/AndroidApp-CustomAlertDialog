fdb41a7dd2407a0ca8f5f8f8c40705b0a6eaa40c
android_shakil <shakil!@#$1234>
2021-05-24 12:52:09 +0600

    Bottom Sheet Dialog

--- Tamim/app/src/main/java/com/dma/tamim/MansionWeddingActivity.java
+++ Tamim/app/src/main/java/com/dma/tamim/MansionWeddingActivity.java
@@ -3,12 +3,24 @@ package com.dma.tamim;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.dma.tamim.Fragment.RatingFragment;
 
 public class MansionWeddingActivity extends AppCompatActivity {
+
+    Button btnBottomSheet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mansion_wedding);
+
+        btnBottomSheet = findViewById(R.id.btnBottomSheet);
+        btnBottomSheet.setOnClickListener(v -> {
+            RatingFragment ratingFragment = RatingFragment.getInstance();
+            ratingFragment.show(getSupportFragmentManager(), "RatingFragment");
+        });
     }
 }
