6e0e954a6fee55f6a4908807ff94d52921cf09a6
android_shakil <shakil!@#$1234>
2021-06-08 23:20:53 +0600

    Update design

--- Tamim/app/src/main/java/com/dma/tamim/MainActivity.java
+++ Tamim/app/src/main/java/com/dma/tamim/MainActivity.java
@@ -7,7 +7,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -67,6 +69,13 @@ public class MainActivity extends AppCompatActivity {
         btnBottomSheetNew.setOnClickListener(v -> {
             BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this, R.style.BottomSheetDialogTheme);
             View bottomSheetView = LayoutInflater.from(this).inflate(R.layout.layout_product_details, findViewById(R.id.bottomSheetContainer));
+
+            Spinner spinner = bottomSheetView.findViewById(R.id.spinner);
+
+            ArrayAdapter<String> myAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+                    getResources().getStringArray(R.array.names));
+            myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(myAdapter);
 
             bottomSheetDialog.setContentView(bottomSheetView);
             bottomSheetDialog.show();
